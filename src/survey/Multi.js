5254c8fc756e50a8d250e3754f142c5449165344
vaibhav jadhav <vaibhav.jadhav.csm@gmail.com>
2021-01-12 20:34:36 +0530

    cool

--- src/survey/Multi.js
+++ src/survey/Multi.js
@@ -11,9 +11,7 @@ const Multi=()=>{
                 direction="row"
                 justify="center"
                 >
-
-
-                    
+        
                 <Grid item>
                     <Paper>
                             <Grid container
